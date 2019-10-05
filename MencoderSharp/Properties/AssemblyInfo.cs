7be154d019412e488ca39c2ab6d5aa9f261cb68f
Stefan Seeland <168659+stesee@users.noreply.github.com>
2019-10-05 14:09:22 +0200

    migrated to .net standard2.0

--- MencoderSharp/Properties/AssemblyInfo.cs
+++ MencoderSharp/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MencoderSharp")]
-[assembly: AssemblyDescription("You need to decode, encode or do some processing stuff on multimedia streams, files or whatever mencoder can read? You want to implement your solution in C# or some other .net language? Here you are! This is an assembly which wrappes calls to e.g. reencode some source with a other codec.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Stefan Seeland")]
-[assembly: AssemblyProduct("MencoderSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2010 GPLv3")]
 [assembly: AssemblyTrademark("seeland.biz")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,14 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("08f03ee4-d38b-48c3-9ba7-2b3d1aacdb26")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
